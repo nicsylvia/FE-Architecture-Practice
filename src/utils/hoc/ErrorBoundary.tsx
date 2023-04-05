2a1e2a39971c1a7f4695c133fe39bef7f6f3df2a
nicsylvia <nicsylvia15f@gmail.com>
2023-04-05 08:28:36 -0700

    saved structure 7

---
+++ src/utils/hoc/ErrorBoundary.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ErrorBoundary = () => {
+  return (
+    <div>
+      <h1>OPPPPPPPPPPPPS!.........</h1>
+
+      <h3>Something went wrong..........</h3>
+    </div>
+  );
+};
+
+export default ErrorBoundary;
