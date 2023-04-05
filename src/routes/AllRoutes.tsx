ae59f7bf75077d5052197b9660448f3c97033af9
nicsylvia <nicsylvia15f@gmail.com>
2023-04-05 08:35:00 -0700

    saved structure 8

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import { HomeLayouts } from "../components";
+import { About, Home } from "../pages";
+import { ErrorBoundary, NotFound } from "../utils";
 
 export const element = createBrowserRouter([
   {
@@ -9,7 +11,22 @@ export const element = createBrowserRouter([
     children: [
       {
         index: true,
+        element: <Home />,
+        errorElement: <ErrorBoundary />,
+        hasErrorBoundary: true,
+      },
+
+      {
+        path: "/about",
+        element: <About />,
+        errorElement: <ErrorBoundary />,
+        hasErrorBoundary: true,
       },
     ],
+  },
+
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
